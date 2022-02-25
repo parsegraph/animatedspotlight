cce7e431628b6c7318ce6c220157a176a2cef998
Aaron Faanes <dafrito@gmail.com>
2022-02-24 22:39:57 -0600

    Upgrade webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
 const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
-  externals: {
-    "parsegraph-checkglerror":{
-      commonjs:"parsegraph-checkglerror",
-      commonjs2:"parsegraph-checkglerror",
-      amd:"parsegraph-checkglerror",
-      root:"parsegraph"
-    }
-  },
   entry: {
     index: relDir("src/index.ts"),
     demo: relDir("src/demo.ts"),
   },
-  ...webpackConfig(true),
+  ...webpackConfig(false),
 };
-
