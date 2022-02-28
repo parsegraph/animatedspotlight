1f57265957f5cc3af132098c960e8242bc64ea75
Aaron Faanes <dafrito@gmail.com>
2022-02-28 01:20:32 -0600

    AnimatedSpotlight implements Projected

--- src/AnimatedSpotlight.ts
+++ src/AnimatedSpotlight.ts
@@ -3,43 +3,52 @@ import Color from "parsegraph-color";
 import Animator from "parsegraph-animator";
 import lerp from "parsegraph-lerp";
 import smoothstep from "parsegraph-smoothstep";
-import { Component } from "parsegraph-window";
+import { Projector, Projected } from "parsegraph-projector";
 import { LayoutNode } from "parsegraph-layout";
-import { Matrix3x3 } from "parsegraph-matrix";
+import { Matrix3x3, matrixIdentity3x3 } from "parsegraph-matrix";
+import Method from 'parsegraph-method';
 
 const FOCUSED_SPOTLIGHT_COLOR = new Color(1, 1, 1, 0.5);
 const FOCUSED_SPOTLIGHT_SCALE = 6;
 
-export default class AnimatedSpotlight {
-  _viewport: Component;
-  _painter: SpotlightPainter;
+export default class AnimatedSpotlight implements Projected {
+  private _painters: Map<Projector, SpotlightPainter>;
   _animator: Animator;
   _fromNode: LayoutNode;
   _toNode: LayoutNode;
   _spotlightColor: Color;
-  constructor(viewport: Component) {
-    this._viewport = viewport;
-    this._painter = null;
+  _world: Matrix3x3;
+  _scheduleUpdate: Method;
+
+  constructor() {
+    this._painters = new Map();
     this._animator = new Animator(480);
     this._fromNode = null;
     this._toNode = null;
     this._spotlightColor = FOCUSED_SPOTLIGHT_COLOR;
+    this._scheduleUpdate = new Method();
+    this._world = matrixIdentity3x3();
   }
 
-  contextChanged(isLost: boolean) {
-    if (this._painter) {
-      this._painter.contextChanged(isLost);
-    }
+  dispose() {
+    this._painters.forEach(painter=>painter.clear());
+    this._painters.clear();
   }
 
-  clear() {
-    if (this._painter) {
-      this._painter.clear();
+  unmount(projector: Projector) {
+    if (!this._painters.has(projector)) {
+      return;
     }
+    this._painters.get(projector).clear();
+    this._painters.delete(projector);
   }
 
   focusedNode() {
     return this._toNode;
+  }
+
+  setColor(color: Color) {
+    this._spotlightColor = color;
   }
 
   getSpotlightScale(node: LayoutNode) {
@@ -55,10 +64,11 @@ export default class AnimatedSpotlight {
     return srad;
   }
 
-  drawNodeFocus() {
+  drawNodeFocus(projector: Projector) {
     const node = this.focusedNode();
     const srad = this.getSpotlightScale(node);
     this.drawSpotlight(
+      projector,
       node.value().getLayout().absoluteX(),
       node.value().getLayout().absoluteY(),
       srad,
@@ -71,27 +81,33 @@ export default class AnimatedSpotlight {
   }
 
   restart(toNode: LayoutNode) {
+    //console.log("Restarting", toNode.state().id());
     this.animator().restart();
     this._fromNode = this._toNode;
     this._toNode = toNode;
-    this._viewport.scheduleUpdate();
+    this.scheduleUpdate();
   }
 
-  drawSpotlight(x: number, y: number, srad: number, color: Color) {
-    if (!this._painter) {
-      this._painter = new SpotlightPainter(this._viewport.window());
+  drawSpotlight(projector: Projector, x: number, y: number, srad: number, color: Color) {
+    //console.log(projector, x, y, srad, color);
+    if (!this.focusedNode()) {
+      return;
     }
-    // console.log(x, y, srad, color);
-    this._painter.drawSpotlight(x, y, srad, color);
+    if (!this._painters.has(projector)) {
+      this._painters.set(projector, new SpotlightPainter(projector.glProvider()));
+    }
+    //console.log(x, y, srad, color);
+    this._painters.get(projector).drawSpotlight(x, y, srad, color);
   }
 
   animating() {
     return this.animator().animating();
   }
 
-  animate(t: number) {
-    if (!this._toNode) {
-      throw new Error("Cannot animate to nothing");
+  animate(projector: Projector, t: number) {
+    if (!this.focusedNode()) {
+      //console.log("Not animating");
+      return;
     }
     t = smoothstep(t);
     let x = this._toNode.value().getLayout().absoluteX();
@@ -101,31 +117,56 @@ export default class AnimatedSpotlight {
       x = lerp(this._fromNode.value().getLayout().absoluteX(), x, t);
       y = lerp(this._fromNode.value().getLayout().absoluteY(), y, t);
       scale = lerp(this.getSpotlightScale(this._fromNode), scale, t);
+      //console.log(this._fromNode.value().getLayout().absoluteX());
+      //console.log(this._toNode.value().getLayout().absoluteX());
+      //console.log(x, y);
     }
-    this.drawSpotlight(x, y, scale, this._spotlightColor);
+    this.drawSpotlight(projector, x, y, scale, this._spotlightColor);
   }
 
-  paint() {
+  tick() {
+    return this.animating();
+  }
+
+  paint(projector: Projector) {
     // console.log("Painting animated spotlight");
-    this.clear();
-    if (!this.animating()) {
-      this.drawNodeFocus();
+    this.unmount(projector);
+    if (!this.animating() && this.focusedNode()) {
+      this.drawNodeFocus(projector);
     }
+    return false;
   }
 
-  render(world: Matrix3x3) {
-    // console.log("Rendering animated spotlight");
+  setWorldTransform(world: Matrix3x3) {
+    this._world = world;
+  }
+
+  render(projector: Projector) {
+    projector.render();
+    const gl = projector.glProvider().gl();
+    gl.viewport(0, 0, projector.width(), projector.height());
+    gl.enable(gl.BLEND);
+    //console.log(projector.width(), projector.height());
+    //console.log("Rendering animated spotlight");
     if (this.animator().animating()) {
-      this.clear();
+      this.unmount(projector);
       const t = this.animator().t();
       if (t >= 1) {
         this.animator().stop();
       }
-      this.animate(t);
+      this.animate(projector, t);
     }
-    if (this._painter) {
-      this._painter.render(world);
+    if (this._painters.has(projector)) {
+      this._painters.get(projector).render(this._world);
     }
     return this.animating();
+  }
+
+  scheduleUpdate() {
+    this._scheduleUpdate.call();
+  }
+
+  setOnScheduleUpdate(cb:()=>void, cbObj?:object) {
+    this._scheduleUpdate.set(cb, cbObj);
   }
 }
